1aac5d3508fe235c3f3b3c4b4bdda0544728fa65
eldorturgunov777 <eldorturgunov777@gmail.com>
2022-06-21 21:29:36 +0500

    Initial commit

---
+++ app/src/main/java/com/example/a631j/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.a631j;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FloatingActionButton mShowDialog = findViewById(R.id.btnShowDialog);
+        mShowDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+                View mView = getLayoutInflater().inflate(R.layout.dialog_page, null);
+                final EditText mEmail = (EditText) mView.findViewById(R.id.etEmail);
+                mBuilder.setPositiveButton("SAVE", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+
+                mBuilder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                mBuilder.setView(mView);
+                final AlertDialog dialog = mBuilder.create();
+                dialog.show();
+
+                dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (!mEmail.getText().toString().isEmpty()) {
+                            Toast.makeText(MainActivity.this,
+                                    "R.string.success_login_msg",
+                                    Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(MainActivity.this, MainActivity.class));
+                            dialog.dismiss();
+                        } else {
+                            Toast.makeText(MainActivity.this,
+                                    "R.string.error_login_msg",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
